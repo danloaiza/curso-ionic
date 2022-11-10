99a826b1750cbaa9eed007e8df4130df3ad75135
danloaiza <daniel.ing.loaiza@gmail.co>
2022-11-09 21:32:23 -0500

    fin sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+  ngOnInit(): void {
+    let roocky = "rocky te amo"
+    roocky = "rocky mk"
+    roocky = "rocky care nalga"
+    debugger;
+    roocky = "rocky feo"
+    console.log("Hola mundo");
+    
+  }
 
 }
